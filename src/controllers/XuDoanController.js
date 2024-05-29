5051d87f37a99506e077b09ec1616ad14918c675
Lamgs <nghiemkhaclam185.mta@gmail.com>
2024-05-29 14:46:38 +0700

    add transsaction for add Xứ đoàn

--- src/controllers/XuDoanController.js
+++ src/controllers/XuDoanController.js
@@ -1,45 +1,42 @@
 const UserModel = require("../models/UserModel")
 const XuDoanModel = require("../models/XuDoanModel")
 const generatePassword = require("../utils/generatePassword")
+const mongoose = require('mongoose')
 
 const XuDoanController = {
     createXuDoan: async (req, res, next) => {
         const body = req.body
+        const session = await mongoose.startSession();
+        session.startTransaction();
         try {
             const newXuDoan =  new XuDoanModel({
                 tenXuDoan: body.tenXuDoan,
                 tenGiaoXu: body.tenGiaoXu
             })
             const password = await generatePassword()
-            const username = await UserModel.countDocuments()
-            await newXuDoan.save()
-                .then(result => {
-                    const newAdminXuDoan = new UserModel({
-                        username: 'admin' + username,
-                        password: password.hashedPassword,
-                        fullName: `Xứ đoàn ${result.tenXuDoan} - Giáo xứ ${result.tenGiaoXu}`,
-                        roleId: 2,
-                        idXuDoan: result._id,
-                    })
-                    return newAdminXuDoan
-                })
-                .then(result => {
-                    result.save()
-                        .then(result2 => {
-                            res.json({
-                                status: true,
-                                data: {
-                                    username: result2.username,
-                                    password: password.realPassword
-                                }
-                            })
-                        })
-                        .catch(e => res.send(e))
-                })
-                .catch(e => {
-                    res.send(e)
-                })
+            const idAccount = await UserModel.countDocuments()
+            const dataXuDoanNew = await newXuDoan.save({session})
+            const newAdminXuDoan = new UserModel({
+                username: 'admin' + idAccount,
+                password: password.hashedPassword,
+                fullName: `Xứ đoàn ${dataXuDoanNew.tenXuDoan} - Giáo xứ ${dataXuDoanNew.tenGiaoXu}`,
+                roleId: 2,
+                idXuDoan: dataXuDoanNew._id,
+            })
+            const dataAdminXuDoanNew = await newAdminXuDoan.save({session})
+            await session.commitTransaction();
+            session.endSession();
+            res.json({
+                status: true,
+                data: {
+                    username: dataAdminXuDoanNew.username,
+                    password: password.realPassword
+                }
+            })
         } catch (error) {
+            await session.abortTransaction();
+            session.endSession();
+            console.log(error)
             res.send(error)
         }
     }
