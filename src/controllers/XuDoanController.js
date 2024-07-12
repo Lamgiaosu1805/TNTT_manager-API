84251fe389158802b4f37ba0f040563a96f8dd21
Lamgs <nghiemkhaclam185.mta@gmail.com>
2024-07-12 17:36:35 +0700

    import excel file

--- src/controllers/XuDoanController.js
+++ src/controllers/XuDoanController.js
@@ -3,6 +3,8 @@ const XuDoanModel = require("../models/XuDoanModel")
 const generatePassword = require("../utils/generatePassword")
 const mongoose = require('mongoose')
 const { SuccessResponse, FailureResponse } = require("../utils/ResponseRequest")
+const ExcelJS = require('exceljs');
+const NganhModel = require("../models/NganhModel")
 
 const XuDoanController = {
     createXuDoan: async (req, res, next) => {
@@ -38,6 +40,61 @@ const XuDoanController = {
             session.endSession();
             console.log(error)
             res.json(FailureResponse("11", error))
+        }
+    },
+    addDoanSinhByExcelFile: async (req, res, next) => {
+        if (!req.file) {
+            return res.status(400).send('No file uploaded.');
+        }
+        else {
+            const session = await mongoose.startSession();
+            session.startTransaction();
+            try {
+                const listNganh = await NganhModel.find()
+                const user = await UserModel.findById(req.user.id)
+                const workbook = new ExcelJS.Workbook();
+                await workbook.xlsx.load(req.file.buffer);
+                const worksheet = workbook.getWorksheet(1); // Lấy worksheet đầu tiên
+                let headers = [];
+                let data = [];
+                worksheet.eachRow((row, rowNumber) => {
+                    if (rowNumber === 1) {
+                        headers = row.values.slice(1); // Bỏ giá trị đầu tiên vì nó là null
+                    } else {
+                        let rowData = {};
+                        row.values.slice(1).forEach((value, index) => {
+                            rowData[headers[index]] = value;
+                        });
+                        data.push(rowData);
+                    }
+                });
+                const listClassBefore = data.map(ele => {
+                    return {
+                        className: ele.Lop,
+                        tenNganh: ele.Nganh
+                    }
+                })
+                const listClassBeforeUnique = Array.from(new Set(listClassBefore.map(JSON.stringify)), JSON.parse);
+                const listClassBeforeData = listClassBeforeUnique.map(ele => {
+                    return {
+                        className: ele.className,
+                        tenNganh: ele.tenNganh,
+                        idXuDoan: user.idXuDoan,
+                        idNganh: listNganh.find(e=> e.tenNganh == ele.tenNganh).id
+                    }
+                });
+                // console.log(listClassBeforeData)
+                await session.commitTransaction();
+                session.endSession();
+                res.json({
+                    total: data.length,
+                    data: data
+                });
+            } catch (error) {
+                await session.abortTransaction();
+                session.endSession();
+                console.log(error)
+            }
         }
     }
 }
